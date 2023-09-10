a6d81917b50506aafb26f4fcbc9e05e2b486a023
Thomas Riffard <ferdodo@hotmail.fr>
2023-09-10 10:42:09 +0200

    rename variables

--- src/find-closest-block.ts
+++ src/find-closest-block.ts
@@ -3,20 +3,20 @@ import { Block, calculateBlockDistance } from "blockwise";
 /**
  * @includeExample ./src/find-closest-block.example.ts
  */
-export function findClosestBlock(points: Block[], target: Block): Block {
-	if (points.length === 0) {
+export function findClosestBlock(blockList: Block[], target: Block): Block {
+	if (blockList.length === 0) {
 		throw new Error("Block list should not be empty !");
 	}
 
-	let closestPoint: Block = points[0];
+	let closestPoint: Block = blockList[0];
 	let closestDistance = calculateBlockDistance(closestPoint, target);
 
-	for (let i = 1; i < points.length; i++) {
-		const currentPoint = points[i];
-		const currentDistance = calculateBlockDistance(currentPoint, target);
+	for (let i = 1; i < blockList.length; i++) {
+		const currentBlock = blockList[i];
+		const currentDistance = calculateBlockDistance(currentBlock, target);
 
 		if (currentDistance < closestDistance) {
-			closestPoint = currentPoint;
+			closestPoint = currentBlock;
 			closestDistance = currentDistance;
 		}
 	}
