ec896275029085d151681d3a8d0e386111fcdb89
Thomas Riffard <ferdodo@hotmail.fr>
2023-09-23 10:45:09 +0200

    add some testing

--- src/find-closest-block.test.ts
+++ src/find-closest-block.test.ts
@@ -1,20 +1,41 @@
 import { Block, findClosestBlock } from "blockwise";
 import assert from "assert/strict";
 
-const blockList: Block[] = [
-	{ x: 10, y: 0, w: 1, h: 1 },
-	{ x: 1, y: 1, w: 1, h: 1 },
-	{ x: 10, y: 0, w: 1, h: 1 }
-];
+{
+	const blockList: Block[] = [
+		{ x: 10, y: 0, w: 1, h: 1 },
+		{ x: 1, y: 1, w: 1, h: 1 },
+		{ x: 10, y: 0, w: 1, h: 1 }
+	];
 
-const targetBlock: Block = { x: 0, y: 1, w: 1, h: 1 };
+	const targetBlock: Block = { x: 0, y: 1, w: 1, h: 1 };
 
-const closestBlock: Block = findClosestBlock(
-	blockList,
-	targetBlock
-);
+	const closestBlock: Block = findClosestBlock(
+		blockList,
+		targetBlock
+	);
 
-assert(closestBlock.x === 1);
-assert(closestBlock.y === 1);
-assert(closestBlock.w === 1);
-assert(closestBlock.h === 1);
+	assert(closestBlock.x === 1);
+	assert(closestBlock.y === 1);
+	assert(closestBlock.w === 1);
+	assert(closestBlock.h === 1);
+}
+
+{
+	const blockList: Block[] = [
+		{ x: 10, y: 0, w: 1, h: 1 },
+		{ x: 1, y: 1, w: 1, h: 1 }
+	];
+
+	const targetBlock: Block = { x: 0, y: 1, w: 1, h: 1 };
+
+	const closestBlock: Block = findClosestBlock(
+		blockList,
+		targetBlock
+	);
+
+	assert(closestBlock.x === 1);
+	assert(closestBlock.y === 1);
+	assert(closestBlock.w === 1);
+	assert(closestBlock.h === 1);
+}
